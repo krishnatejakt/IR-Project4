ab16ab1566750296ab8b49d2877bd1f76b4b1e1b
krishnatejakt <krishnatejamellacheruvu@gmail.com>
2020-12-07 22:29:40 -0500

    Latest Code.
    Pushed the latest code to main,  from here I will push the code to main.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IR-Project4
+
+Please upload the json files in flaskblog/json_files and create subfolders for country, poi, and other tweets accordingly.
---
+++ flaskblog/forms.py
@@ -0,0 +1,28 @@
+from wtforms import Form, StringField, SelectField, SubmitField
+from flask_wtf import FlaskForm
+
+# class POISearchForm(Form):
+#     choices = [('Modi', 'Modi'),
+#                ('Trump', 'Trump'),
+#                ('Amit', 'Amit')]
+#     select = SelectField('Search for POI:', choices=choices)
+#     search = StringField('')
+
+class covid(FlaskForm):
+    search = StringField('searchbar')
+    button_submit = SubmitField('Search')
+
+class poi_names(FlaskForm):
+    choices = [('',''),('narendramodi','narendramodi'), ('VP','VP'),('joebiden','joebiden'),('pmoindia','pmoindia'),('smritiirani','smritiirani'),('realdonaldtrump','realdonaldtrump'),('AmitShah','AmitShah'),('matteosalvinimi','matteosalvinimi'),('cdcgov','cdcgov'),('ewarren','ewarren'),('giuseppeconte','giuseppeconte'),('nygovcuomo','nygovcuomo')]
+    select = SelectField('Search for POI:', choices=choices)
+    button_submit = SubmitField('Search')
+
+class country(FlaskForm):
+    choices = [('',''),('India','India'),('USA','USA'),('Italy','Italy')]
+    select = SelectField('Search for POI:', choices=choices)
+    button_submit = SubmitField('Search')
+
+class topic(FlaskForm):
+    topics = [('',''),('a','a'),('b','b')]
+    select = SelectField('Search for POI:', choices=topics)
+    button_submit = SubmitField('Search')
