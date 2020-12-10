2945edc6229f0e6ba3e9f1ace512b1edd1f28db6
krishnatejakt <krishnatejamellacheruvu@gmail.com>
2020-12-10 18:30:07 -0500

    updated

--- flaskblog/newsapi.py
+++ flaskblog/newsapi.py
@@ -6,6 +6,7 @@ def news(query):
     # news_query = requests.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+query+'&api-key=KsLEcgWEJLqIkK2hdacjklZiPyHyOxLh&sort=newest&page=0') # (your url)
     # data = news_query.json()
     # return data['response']['docs']
+    # return data['response']['docs']
 
     if(query!='Trending'):
         url = ('http://newsapi.org/v2/everything?'
