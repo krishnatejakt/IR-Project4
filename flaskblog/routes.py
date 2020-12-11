4c51fbdb91ed87d6c5b8a53515115eaaee7c8cf4
krishnatejakt <krishnatejamellacheruvu@gmail.com>
2020-12-10 23:24:21 -0500

    random

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -36,19 +36,19 @@ query_news = []
 @app.route('/home')
 
 def home():
-        data = urlopen('http://'+ip_address+':8983/solr/IRF20P1/select?q=*%3A*&sort=influencer_score%20desc%2C%20score%20desc&wt=json&rows=15')
-        posts = json.load(data)['response']['docs']
-        youtube_term = youtube_query('Trending')
-        x = []
-        for element in posts:
-            name = element['user.screen_name'][0]
-            element['user_name'] = name
-            element['profile_image'] = element['user.profile_image_url'][0]
-            x.append(element)
-            element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
+    data = urlopen('http://'+ip_address+':8983/solr/IRF20P1/select?q=*%3A*&sort=influencer_score%20desc%2C%20score%20desc&wt=json&rows=15')
+    posts = json.load(data)['response']['docs']
+    youtube_term = youtube_query('Trending')
+    x = []
+    for element in posts:
+        name = element['user.screen_name'][0]
+        element['user_name'] = name
+        element['profile_image'] = element['user.profile_image_url'][0]
+        x.append(element)
+        element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
 
-            query_news = news('Trending')
-        return render_template('home.html',posts1='Trending Posts!',posts=posts,tests=query_news,youtube=youtube_term)
+        query_news = news('Trending')
+    return render_template('home.html',posts1='Trending Posts!',posts=posts,tests=query_news,youtube=youtube_term)
     
 @app.route('/about')
 
@@ -56,13 +56,10 @@ def about():
     return render_template('about.html')
 
 @app.route('/search',methods=['GET','POST'])
+
 def search():
     gs = google_translator()
     form = covid()
-    form1 = country()
-    form2 = poi_names()
-    form3 = topic()
-
     if form.validate_on_submit():
 
         youtube_search_term = ''
@@ -71,11 +68,14 @@ def search():
             query_term='\"\"'
             query_term_dup = '\"\"'
         else:
+            #print(type(form.search.data))
             query_term = form.search.data
             flash(f'Results for {form.search.data}!','success')
             query_term_dup = gs.translate(query_term,'en')
             query_term = query_term.replace(' ','%20')
-            query_term = query_term.encode("raw-unicode-escape").decode("utf-8")
+            query_term = query_term.encode("raw-unicode-escape").decode("utf-8")        
+            query_term = '\"' + query_term + '\"'
+    
             #query_term = query_term.encode('utf-8')
 
         if(form.select.data==''):
@@ -103,10 +103,12 @@ def search():
         youtube_term = youtube_query(youtube_search_term)
         query_news = news(youtube_search_term)
 
-        print(youtube_search_term)
+        youtube_search_term = '\"' + youtube_search_term + '\"'
+
+        #print(youtube_search_term)
 
         data = urlopen('http://'+ip_address+':8983/solr/IRF20P1/select?defType=edismax&q=full_text%3A'+query_term+'%20AND%20user.screen_name%3A'+poi+'%20AND%20country%3A'+location+'&qf=full_text&sort=influencer_score%20desc%2C%20score%20desc&stopwords=true&rows=15&wt=json')
-        print('http://'+ip_address+':8983/solr/IRF20P1/select?defType=edismax&q=full_text%3A'+query_term+'%20AND%20user.screen_name%3A'+poi+'%20AND%20country%3A'+location+'&qf=full_text&sort=influencer_score%20desc%2C%20score%20desc&stopwords=true&wt=json&rows=15')
+        #print('http://'+ip_address+':8983/solr/IRF20P1/select?defType=edismax&q=full_text%3A'+query_term+'%20AND%20user.screen_name%3A'+poi+'%20AND%20country%3A'+location+'&qf=full_text&sort=influencer_score%20desc%2C%20score%20desc&stopwords=true&wt=json&rows=15')
         posts = json.load(data)['response']['docs']
 
 
@@ -123,73 +125,10 @@ def search():
             element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
 
         return render_template('home.html',posts=posts,tests=query_news,youtube=youtube_term)
-
-
-
-    #     if(form.search.data!=''):
-    #         flash(f'Results for {form.search.data}!','success')
-    #         x = []
-    #         query_term = form.search.data
-    #         query_term = query_term.replace(' ','%20')
-
-    #         data = urlopen('http://'+ip_address+':8983/solr/IRF20P4/select?defType=edismax&q=full_text%3A'+query_term+'&rows=15&sort=influencer_score%20desc&stopwords=true&wt=json')
-    #         posts = json.load(data)['response']['docs']
-
-    #         youtube_term = youtube_query(query_term)
-
-    #         for element in posts:
-    #             name = element['user'][0]
-    #             name = json.loads(json.dumps(ast.literal_eval(name)))['name']
-    #             element['user_name'] = name
-    #             x.append(element)
-    #             element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
-
-    #         query_news = news(query_term)
-
-    #         return render_template('home.html',posts=posts,tests=query_news,youtube=youtube_term)
-
-    # if form1.validate_on_submit():
-    #     if(form1.select.data!=''):
-    #         flash(f'Results for {form1.select.data}!','success')
-    #         query_term = form1.select.data
-
-    #         x = []
-    #         data = urlopen('http://'+ip_address+':8983/solr/IRF20P4/select?defType=edismax&q=country%3A'+form1.select.data+'&rows=15&sort=influencer_score%20desc&stopwords=true&wt=json')
-    #         posts = json.load(data)['response']['docs']
-    #         for element in posts:
-    #             name = element['user'][0]
-    #             name = json.loads(json.dumps(ast.literal_eval(name)))['name']
-    #             element['user_name'] = name
-    #             x.append(element)
-    #             element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
-
-    #         query_news = news(form1.select.data)
-    #         youtube_term = youtube_query(query_term)
-
-    #         return render_template('home.html',posts=posts,tests=query_news,youtube=youtube_term)
-            
-    # if(form2.validate_on_submit()):
-    #     if(form2.select.data!=''):
-    #         flash(f'Results for {form2.select.data}!','success')
-    #         x = []
-    #         query_term = form2.select.data
-    #         data = urlopen('http://'+ip_address+':8983/solr/IRF20P4/select?defType=edismax&q=user%3A'+form2.select.data+'&rows=15&sort=influencer_score%20desc&stopwords=true&wt=json')
-    #         posts = json.load(data)['response']['docs']
-    #         for element in posts:
-    #             name = element['user'][0]
-    #             name = json.loads(json.dumps(ast.literal_eval(name)))['name']
-    #             element['user_name'] = name
-    #             x.append(element)
-    #             element["sentiment_score"] = sentiment_score(element['full_text'][0].lower())
-
-    #         query_news = news(form2.select.data)
-    #         youtube_term = youtube_query(query_term)
-
-    #         return render_template('home.html',posts=posts,tests=query_news,youtube=youtube_term)
             
         
 
-    return render_template('search.html',form=form,form1=form1,form2=form2)
+    return render_template('search.html',form=form)
 
 @app.route('/visualization')
 
